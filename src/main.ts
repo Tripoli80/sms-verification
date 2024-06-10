4e4f074c08e092693f2ab997950b2bbfd5810e63
Tripoli80 <35936980+Tripoli80@users.noreply.github.com>
2024-06-10 12:13:32 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:8000', 'https://dev.mrouter.com.ua'], // разрешаем доступ только с этого источника
+    origin: [
+      'http://localhost:8000',
+      'https://dev.mrouter.com.ua',
+      'https://mrouter.com.ua',
+    ], // разрешаем доступ только с этого источника
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // разрешенные методы
     credentials: true, // разрешаем отправлять и принимать куки на кросс-доменных запросах
   });
